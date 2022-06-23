e3679470432a8aa7a1cfec7c4922661de29a8d3a
Kayanski <kowalski.kowalskin@gmail.com>
2022-06-23 11:25:42 +0200

    Rationalized the nft migrator

--- nft_migrator/src/index.ts
+++ nft_migrator/src/index.ts
@@ -10,8 +10,7 @@ import {
   LCDClient,
 } from '@terra-money/terra.js';
 
-let env = require("../env.json")
-let global_env = require("../env.json");
+let globalEnv = require("../env.json");
 
 const PORT = 8080;
 
@@ -41,33 +40,33 @@ app.use(function (_req, res, next) {
 
 let mnemonics = require("../mnemonics.json")
 
-let escrow_handler = new Address(env["classic"],mnemonics.escrow.mnemonic);
+let escrowHandler = new Address(globalEnv["classic"],mnemonics.escrow.mnemonic);
 
 
-async function try_send_token_2_0(contract_info: any, user_address: string, token_id: string){
+async function trySendToken2_0(contractInfo: any, userAddress: string, tokenId: string){
   // We try to send the token.
-  let nft_address_2 = contract_info.contract2;
-  let nft_address_1 = contract_info.contract1;
-  let terra2Mnemonic = mnemonics[nft_address_1].mnemonic;
-  let nft_handler = new Address(env["staging"],terra2Mnemonic);
-  let nft_2_contract = nft_handler.getContract(nft_address_2);
-  let escrow = escrow_handler.getContract(contract_info.escrow_contract)
+  let nftAddress2 = contractInfo.contract2;
+  let nftAddress1 = contractInfo.contract1;
+  let terra2Mnemonic = mnemonics[nftAddress1].mnemonic;
+  let nftHandler = new Address(globalEnv["staging"],terra2Mnemonic);
+  let nft2Contract = nftHandler.getContract(nftAddress2);
+  let escrow = escrowHandler.getContract(contractInfo.escrow_contract)
   let migrated = false;
 
-  console.log(nft_address_2, {
-    recipient: user_address,
-    token_id,
+  console.log(nftAddress2, {
+    recipient: userAddress,
+    token_id: tokenId,
   })
   
   // We execute the transfer on Terra 2.0
-  await nft_2_contract.execute.transfer_nft({
-    recipient: user_address,
-    token_id,
+  await nft2Contract.execute.transfer_nft({
+    recipient: userAddress,
+    token_id: tokenId
   })
   .then(async (_response: any)=>{
     // We indicate the token has been migrated
     await escrow.execute.migrated({
-      token_id
+      token_id: tokenId
     })
     migrated = true;
     
@@ -84,62 +83,57 @@ async function try_send_token_2_0(contract_info: any, user_address: string, toke
 async function main() {
 
   app.get('/migrator/contract_list', async (_req: any, res: any) => {
-      let contract_list = require('../nft_contracts.json');
-      await res.status(200).send(contract_list);
+      let contractList = require('../nft_contracts.json');
+      await res.status(200).send(contractList);
   });
 
 
-  app.get('/migrator/migrate/:address/:contract/:token_id', async (req: any, res: any) => {
+  app.get('/migrator/migrate/:address/:contract/:tokenId', async (req: any, res: any) => {
       const address = req.params.address;
       const contract = req.params.contract;
-      const token_id = req.params.token_id;
+      const tokenId = req.params.tokenId;
 
       // We verify the contract is registered with the api
-      let contract_list = require('../nft_contracts.json');
-      let contract_info = contract_list[contract];
-      if(!contract_info){
+      let contractList = require('../nft_contracts.json');
+      let contractInfo = contractList[contract];
+      if(!contractInfo){
         await res.status(404).send("Contract was not registered with this api");
         return;
       }
 
       // We query the Terra 1.0 chain to make sure the designated NFT has been deposited by the address in the escrow contract
-      let terra = new LCDClient(global_env["classic"]['chain']);
-      let response: any = await terra.wasm.contractQuery(
-        contract_info.escrow_contract,
+      let terra = new LCDClient(globalEnv["classic"]['chain']);
+      await terra.wasm.contractQuery(
+        contractInfo.escrow_contract,
         {
           depositor:{
-            token_id
+            token_id: tokenId
           }
         }
-      ).then(async (response: any)=>{
+      ).then((response: any)=>{
         // If there is a response, we check it matches the info sent
-        if(response?.token_id != token_id){
-          await res.status(404).send("Token not deposited");
-          return
-
+        if(response?.token_id != tokenId){
+          throw Error("Token not deposited");
         }else if(response.depositor != address){
-          await res.status(404).send("Token not deposited by the indicated address");
-          return
+          throw Error("Token not deposited by the indicated user");
         }else if(response.migrated){
-          await res.status(404).send("Token already migrated");
-          return
+          throw Error("Token already migrated");
         }
         // We try to send the token to the depositor on the Terra 2.0 chain
-        if(await try_send_token_2_0(contract_info, address, token_id)){
-          await res.status(200).send("Your Token was migrated successfuly");
+        return trySendToken2_0(contractInfo, address, tokenId);
+      }).then((migrated)=>{
+        if(migrated){
+          return res.status(200).send("Your Token was migrated successfuly");
         }else{
-          await res.status(503).send("Your token couldn't be migrated for now, it is locked in the escrow contract waiting to be migrated, try again later");
+          return res.status(503).send("Your token couldn't be migrated for now, it is locked in the escrow contract waiting to be migrated, try again later");
         }
-
-      }).catch(async (error) =>{
-        console.log(error)
-          await res.status(404).send({
-            error_text:"Error occured while migrating the token. Please report the error to the project owner", 
+      })
+      .catch((error) =>{
+          return res.status(404).send({
+            error_text:"Error occured while migrating the token", 
             error: error.message
           });
       });
-      
-
   });
 
   if (process.env.EXECUTION == 'PRODUCTION') {
